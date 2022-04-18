d45359f08bc98ff3b37c2c976e0afcee22d5e1ff
Andrei Gramakov <mail@agramakov.me>
2022-04-18 15:02:42 +0200

    Add a script to access remote brain_pycore and ssh keys

---
+++ Open_brain_pycore.ps1
@@ -0,0 +1,2 @@
+code --file-uri=vscode-remote://ssh-remote+mind@zakhar/zakhar/python_packages/brain_pycore/brain_pycore.code-workspace
+
---
+++ Open_brain_service.ps1
@@ -0,0 +1,2 @@
+code --file-uri=vscode-remote://ssh-remote+mind@zakhar/zakhar/service/zakhar_service.code-workspace
+
